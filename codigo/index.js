bba3037c6a8a6a38dea264a2e825261a528d4763
Gustavo Henrique dos Santos Dias <1543570@sga.pucminas.br>
2024-12-08 23:06:58 -0300

    Add files via upload

--- codigo/index.js
+++ codigo/index.js
@@ -1,5 +1,5 @@
 // Trabalho Interdisciplinar 1 - Aplicações Web
-// 
+//
 // Esse módulo implementa uma API RESTful baseada no JSONServer
 // O servidor JSONServer fica hospedado na seguinte URL
 // https://jsonserver.rommelpuc.repl.co/contatos
@@ -12,19 +12,17 @@
 //
 // Autor: Rommel Vieira Carneiro
 // Data: 03/10/2023
-// teste
-
 
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./db/db.json')
-  
+
 // Para permitir que os dados sejam alterados, altere a linha abaixo
 // colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults({ noCors: true })
+const middlewares = jsonServer.defaults()
+
 server.use(middlewares)
 server.use(router)
-
 server.listen(3000, () => {
-  console.log(`JSON Server is running em http://localhost:3000`)
+  console.log('JSON Server está em execução!')
 })
